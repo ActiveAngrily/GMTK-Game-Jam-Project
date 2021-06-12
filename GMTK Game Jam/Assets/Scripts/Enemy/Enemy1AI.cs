0d451290dc9640366bd866e067b885fd1394c558
ActiveAngrily <45509422+ActiveAngrily@users.noreply.github.com>
2021-06-12 13:28:16 +0530

    Added Enemy1 AI and bugfix

--- GMTK Game Jam/Assets/Scripts/Enemy/Enemy1AI.cs
+++ GMTK Game Jam/Assets/Scripts/Enemy/Enemy1AI.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class EnemyAI : MonoBehaviour
-{
-    /*
+using Pathfinding;
+/*
      * Okay, so diving the development of this script into three phases, but first
      * What I want the enemy to do -
      *    Wait idle for 3 seconds
      *    Zap to another place(let's oscillate to 2 places)
      *    shoot while idle
-     */
+*/
+public class Enemy1AI : MonoBehaviour
+{
+    /*
+    
     public enum State
     {
         Entering,
@@ -93,5 +95,80 @@ public class EnemyAI : MonoBehaviour
         Vector2 randomDirection = new Vector2(Random.Range(0,1),Random.Range(0,1)).normalized;
         transform.Translate(randomDirection * zapSpeed);
     }
+
+    */
+
+    Rigidbody2D rb;
+    //The point to move to
+    [SerializeField]
+    private Transform[] targetPoint;
+    private Seeker seeker;
+    //The calculated path
+    public Path path;
+    //The AI's speed per second
+    public float speed = 100;
+    //The max distance from the AI to a waypoint for it to continue to the next waypoint
+    public float nextWaypointDistance = 3;
+    //The waypoint we are currently moving towards
+    private int currentWaypoint = 0;
+
+    public float pathgenerationRate = 5f;
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        seeker = GetComponent<Seeker>();
+        // controller = GetComponent<CharacterController>();
+        //Start a new path to the targetPosition, return the result to the OnPathComplete function
+
+
+        InvokeRepeating("GenerateNewPath", 0f, pathgenerationRate);
+    }
+    int i = 0;
+    private void GenerateNewPath()
+    {
+        Debug.Log(i);
+        if(i < targetPoint.Length-1)
+        {   
+            seeker.StartPath(transform.position, targetPoint[i].localPosition, OnPathComplete);
+            i++;
+        } else{
+            i = 0;
+            seeker.StartPath(transform.position, targetPoint[i].localPosition, OnPathComplete);
+        }
+    }
+    public void OnPathComplete(Path p)
+    {
+       // Debug.Log("Yay, we got a path back. Did it have an error? " + p.error);
+        if (!p.error)
+        {
+            path = p;
+            //Reset the waypoint counter
+            currentWaypoint = 0;
+        }
+    }
+    public void FixedUpdate()
+    {
+        if (path == null)
+        {
+            //We have no path to move after yet
+            return;
+        }
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+       //     Debug.Log("End Of Path Reached");
+            return;
+        }
+        //Direction to the next waypoint
+        Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized;
+        dir *= speed * Time.deltaTime;
+        rb.AddForce(dir);
+
+        //Check if we are close enough to the next waypoint
+        //If we are, proceed to follow the next waypoint
+        if (Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]) < nextWaypointDistance)
+        {
+            currentWaypoint++;
+            return;
+        }
+    }
 }
-    
