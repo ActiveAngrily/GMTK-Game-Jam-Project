acf42250bc26e4e9c90644291c25796a297c4c20
Bendeguz Anisics <bendixer.softwares@gmail.com>
2021-06-12 10:49:26 +0200

    added static instance field to gamemanager monobehaviour

--- GMTK Game Jam/Assets/GameManager.cs
+++ GMTK Game Jam/Assets/GameManager.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
     public Transform PlayerTransform { get; private set; }
 
     [SerializeField] GameObject player;
